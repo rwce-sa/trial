c69340be92910cf957e5a13cb5ae812b6b3ae48b
kistlaszlo <laszlo.kis.toth@ericsson.com>
2016-06-02 14:45:12 +0200

    Create deploy_on_apcera.sh

---
+++ deploy_on_apcera.sh
@@ -0,0 +1,17 @@
+cd /var/jenkins_home/
+./apc logout
+./apc login
+sleep 1
+echo elszlki
+sleep 1
+echo Elszlki123
+sleep 1
+./apc app create $appname
+echo /var/jenkins_home/jobs/blabla/workspace/
+sleep 1
+echo 1
+sleep 1
+echo 256
+sleep 1
+echo "y"
+sleep 5
